45c8c34618eaa5212b7da185aff43e846854ecee
Kartik <kartik3344@gmail.com>
2025-08-07 14:14:07 +0530

    fixes to timeline component

--- components/Journey.tsx
+++ components/Journey.tsx
@@ -19,8 +19,6 @@ export default function Journey(){
                 Microsoft & Telco-IoT CoE
             </h1>
           <p className="mb-8 text-md font-normal text-neutral-800 md:text-lg ">
-            {/* <h2 className="text-lg md:text-4xl mb-4 text-black max-w-4xl">
-        </h2> */}
             Launched new Centers of Excellence focused on Microsoft technologies and Telco-IoT (Internet of Things) solutions. This step diversified the company’s technology stack and positioned it for leadership in smart solutions, cloud computing, and next-gen telecom innovations.
           </p>
           <div className="grid grid-cols-2 gap-4 text-white">
@@ -29,7 +27,7 @@ export default function Journey(){
               alt="2024"
               width={500}
               height={500}
-              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-full lg:h-full"
             />
           </div>
         </div>
@@ -39,10 +37,10 @@ export default function Journey(){
       title: "2023",
       content: (
         <div>
-          <p className="mb-8 text-xs font-normal text-neutral-800 md:text-sm ">
+          <h1 className="text-lg md:text-4xl mb-4 text-black max-w-4xl">
             India Offshore Center
-          </p>
-          <p className="mb-8 text-xs font-normal text-neutral-800 md:text-sm ">
+          </h1>
+          <p className="mb-8 text-md font-normal text-neutral-800 md:text-lg ">
             A full-fledged offshore delivery center was set up in India to support global clients with 24/7 services. This marked a significant upgrade in operational capacity and reinforced India’s role as a strategic technology hub for the company.
           </p>
           <div className="grid grid-cols-2 gap-4">
@@ -51,7 +49,7 @@ export default function Journey(){
               alt="hero template"
               width={500}
               height={500}
-              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-full lg:h-full"
             />
           </div>
         </div>
@@ -61,10 +59,10 @@ export default function Journey(){
       title: "2022",
       content: (
         <div>
-          <p className="mb-4 text-xs font-normal text-neutral-800 md:text-sm ">
+          <h1 className="text-lg md:text-4xl mb-4 text-black max-w-4xl">
             SAP & Zoho CoE (Center of Excellence)
-          </p>
-          <p className="mb-4 text-xs font-normal text-neutral-800 md:text-sm ">
+          </h1>
+          <p className="mb-8 text-md font-normal text-neutral-800 md:text-lg ">
                 Established dedicated Centers of Excellence for SAP and Zoho solutions, offering enterprise clients specialized implementation, integration, and support services. These CoEs positioned the company as a trusted partner in ERP and business software ecosystems.
           </p>
           <div className="grid grid-cols-2 gap-4">
@@ -73,7 +71,7 @@ export default function Journey(){
               alt="hero template"
               width={500}
               height={500}
-              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-full lg:h-full"
             />
           </div>
         </div>
@@ -83,10 +81,10 @@ export default function Journey(){
       title: "2021",
       content: (
         <div key={Math.random()*100}>
-          <p className="mb-4 text-xs font-normal text-neutral-800 md:text-sm ">
+          <h1 className="text-lg md:text-4xl mb-4 text-black max-w-4xl">
             USA & Germany Operations
-          </p>
-          <p className="mb-4 text-xs font-normal text-neutral-800 md:text-sm ">
+          </h1>
+          <p className="mb-8 text-md font-normal text-neutral-800 md:text-lg ">
                 The company expanded its footprint into Western markets by launching operations in the USA and Germany. This broadened the client base, allowed closer proximity to key customers, and enhanced its global delivery capabilities.
           </p>
           <div className="grid grid-cols-2 gap-4">
@@ -95,7 +93,7 @@ export default function Journey(){
               alt="hero template"
               width={500}
               height={500}
-              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-full lg:h-full"
             />
           </div>
         </div>
@@ -105,10 +103,10 @@ export default function Journey(){
       title: "2020",
       content: (
         <div key={2020}>
-          <p className="mb-4 text-xs font-normal text-neutral-800 md:text-sm ">
+          <h1 className="text-lg md:text-4xl mb-4 text-black max-w-4xl">
             India Sourcing Operations
-          </p>
-          <p className="mb-4 text-xs font-normal text-neutral-800 md:text-sm ">
+          </h1>
+          <p className="mb-8 text-md font-normal text-neutral-800 md:text-lg ">
                 India operations were initiated to leverage cost-effective talent and robust IT capabilities. This move enabled global sourcing, strengthened backend operations, and created a scalable offshore delivery model.
           </p>
           <div className="grid grid-cols-2 gap-4">
@@ -117,7 +115,7 @@ export default function Journey(){
               alt="hero template"
               width={500}
               height={500}
-              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-full lg:h-full"
             />
           </div>
         </div>
@@ -127,10 +125,10 @@ export default function Journey(){
       title: "2019",
       content: (
         <div key={2019}>
-          <p className="mb-4 text-xs font-normal text-neutral-800 md:text-sm ">
+          <h1 className="text-lg md:text-4xl mb-4 text-black max-w-4xl">
             Digital Platform Launched
-          </p>
-          <p className="mb-4 text-xs font-normal text-neutral-800 md:text-sm ">
+          </h1>
+          <p className="mb-8 text-md font-normal text-neutral-800 md:text-lg ">
                 A major product milestone: the launch of the company’s digital platform. This enabled automation, scalability, and enhanced digital services, forming the core infrastructure to support global operations and digital transformation initiatives for clients.
           </p>
           <div className="grid grid-cols-2 gap-4">
@@ -139,7 +137,7 @@ export default function Journey(){
               alt="hero template"
               width={500}
               height={500}
-              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-full lg:h-full"
             />
           </div>
         </div>
@@ -149,10 +147,10 @@ export default function Journey(){
       title: "2018",
       content: (
         <div key={2018}>
-          <p className="mb-4 text-xs font-normal text-neutral-800 md:text-sm ">
+          <h1 className="text-lg md:text-4xl mb-4 text-black max-w-4xl">
             IT Corridors in China
-          </p>
-          <p className="mb-4 text-xs font-normal text-neutral-800 md:text-sm ">
+          </h1>
+          <p className="mb-8 text-md font-normal text-neutral-800 md:text-lg ">
                 Expansion began into mainland China with the establishment of IT corridors, enabling the company to tap into the region&apos;s vast tech talent pool and strengthen its delivery capabilities in the Asian market.
           </p>
           <div className="grid grid-cols-2 gap-4">
@@ -161,7 +159,7 @@ export default function Journey(){
               alt="hero template"
               width={500}
               height={500}
-              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-full lg:h-full"
             />
           </div>
         </div>
@@ -171,10 +169,10 @@ export default function Journey(){
       title: "2017",
       content: (
         <div key={2017}>
-          <p className="mb-4 text-xs font-normal text-neutral-800 md:text-sm ">
+          <h1 className="text-lg md:text-4xl mb-4 text-black max-w-4xl">
             Founded in HONG KONG
-          </p>
-          <p className="mb-4 text-xs font-normal text-neutral-800 md:text-sm ">
+          </h1>
+          <p className="mb-8 text-md font-normal text-neutral-800 md:text-lg ">
                 The company was established in Hong Kong, marking its official entry into the global market. This foundational year laid the groundwork for future growth and innovation, focusing on international business strategy and service offerings.
           </p>
           <div className="grid grid-cols-2 gap-4">
@@ -183,7 +181,7 @@ export default function Journey(){
               alt="hero template"
               width={500}
               height={500}
-              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-full lg:h-full"
             />
           </div>
         </div>
