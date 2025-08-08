51f6c83f34a3b977778cb390a4ca1fe98b419c80
Kartik <kartik3344@gmail.com>
2025-08-08 21:19:42 +0530

    updated bg color

--- components/Industries.tsx
+++ components/Industries.tsx
@@ -2,7 +2,7 @@ import { IndustryCards } from "./ui/industries";
 
 export default function Industries(){
     return ( 
-        <section className="flex flex-col items-center justify-center gap-8 bg-linear-to-b from-indigo-900 to-indigo-300 p-15 py-15 h-full w-screen">
+        <section className="flex flex-col items-center justify-center gap-8 bg-linear-to-b from-indigo-950 to-purple-900 p-15 py-15 h-full w-screen">
             <div className="flex flex-col gap-5 text-white">
                 <h1 className="text-center text-4xl font-bold">Industries</h1>
                 <h2 className="text-md font-extralight tracking-wide">Transforming Industries with Cutting-Edge Technology</h2>
