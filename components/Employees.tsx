a85571e3984de1814fda0fb0343c049fb184c34c
Kartik <kartik3344@gmail.com>
2025-08-09 12:56:44 +0530

    fixed employees page

--- components/Employees.tsx
+++ components/Employees.tsx
@@ -13,27 +13,27 @@ export default function Employees(){
             <div className="text-center mb-5">
                 <h1 className="text-4xl font-bold">Our Employees</h1>
             </div>
-            <div className="grid grid-cols-3 gap-10">
-                <div className="flex items-center justify-center border py-5 px-2 rounded-4xl border-indigo-500 shadow-lg">
-                    <Image alt="gender-identity" src={gender}/>
-                    <h1 className="text-lg font-bold tracking-tight p-2 mz-2">Gender Identity</h1>
+            <div className="grid grid-cols-1 xl:grid xl:grid-cols-3 md:grid md:grid-cols-2 gap-10">
+                <div className="flex items-center justify-center border py-5 px-2 rounded-4xl border-indigo-500 shadow-lg min-w-sm">
+                    <Image alt="gender-identity" src={gender} className="w-30 xl:w-80 lg:w-50 md:40"/>
+                    <h1 className="text-lg font-bold tracking-tight p-2 mx-2">Gender Identity</h1>
                 </div>
-                <div className="flex items-center justify-center border py-5 px-2 rounded-4xl border-indigo-500 shadow-lg">
-                    <Image alt="national-origin" src={national_origin}/>
+                <div className="flex items-center justify-center border py-5 px-2 rounded-4xl border-indigo-500 shadow-lg min-w-sm">
+                    <Image alt="national-origin" src={national_origin} className="w-30 xl:w-80 lg:w-50 md:40"/>
                     <h1 className="text-lg font-bold tracking-tight p-2">National Origin</h1>
                 </div>
-                <div className="flex items-center justify-center border py-5 px-2 rounded-4xl border-indigo-500 shadow-lg">
-                    <Image alt="experience" src={experience}/>
+                <div className="flex items-center justify-center border py-5 px-2 rounded-4xl border-indigo-500 shadow-lg min-w-sm">
+                    <Image alt="experience" src={experience} className="w-30 xl:w-80 lg:w-50 md:40"/>
                     <h1 className="text-lg font-bold tracking-tight p-2 ml-2">Experience</h1>
                 </div>
             </div>
-            <div className="grid grid-cols-2 gap-10 items-center justify-center">
-                <div className="flex items-center justify-center border p-7 rounded-4xl border-indigo-500 shadow-lg">
-                    <Image alt="education" src={education}/>
+            <div className="grid lg:grid-cols-2 gap-10 items-center justify-center">
+                <div className="flex items-center justify-center border py-10 px-2 rounded-4xl border-indigo-500 shadow-lg min-w-sm">
+                    <Image alt="education" src={education} className="w-30 xl:w-80 lg:w-50 md:40"/>
                     <h1 className="text-lg font-bold tracking-tight p-2">Education</h1>
                 </div>
-                <div className="flex items-center justify-center border p-7 rounded-4xl border-indigo-500 shadow-lg">
-                    <Image alt="affirmative" src={affirmative}/>
+                <div className="flex items-center justify-center border py-5 px-2 rounded-4xl border-indigo-500 shadow-lg min-w-sm">
+                    <Image alt="affirmative" src={affirmative} className="w-30 xl:w-80 lg:w-50 md:40"/>
                     <h1 className="text-lg font-bold tracking-tight p-2">Affirmative Action</h1>
                 </div>
             </div>
