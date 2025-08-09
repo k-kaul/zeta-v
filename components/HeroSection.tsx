70b3bbb72d073ee210df45d63efea030340c3100
Kartik <kartik3344@gmail.com>
2025-08-09 17:06:27 +0530

    added dot background to hero section

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,33 +1,47 @@
 import { MoveRight } from "lucide-react"
 import Link from "next/link"
 import LogoSlider from "./ui/logoslider"
+import { DotBackgroundDemo } from "./ui/dotbackground"
+import { cn } from "@/lib/utils"
 
 export default function HeroSection(){
     return (
-        <section className="flex flex-col py-30 gap-6 w-full h-full bg-gray-100">
-            <div className="flex flex-col gap-8 items-center">
-                <h1 className="text-center xl:text-7xl text-6xl font-extrabold tracking-tight font-sans max-w-2xl">
-                    Democratizing IT {' '}                    
-                    <span className="bg-gradient-to-r from-blue-700 via-purple-700 to-red-700 bg-clip-text text-transparent">Beyond Barriers</span>
-                </h1>    
-                <h2 className="text-center font-light text-gray-900 tracking-wider">
-                    We believe in making technology accessible to everyone,<br></br> regardless of location, expertise, or resources.
-                </h2>
-                <div className="flex flex-row gap-5 justify-center font-bold">
-                    <Link href={'/#aboutus'}>
-                        <button className="flex bg-blue-600 py-2 px-5 rounded-full text-white cursor-pointer hover:scale-105 duration-150">About Us <MoveRight className="pl-2"/></button>
-                    </Link>
-                    <Link href={'/contact'}>
-                        <button className="flex bg-gray-100 py-2 px-5 rounded-full text-black cursor-pointer hover:scale-105 duration-150">Contact Us <MoveRight className="pl-2"/></button>
-                    </Link>
-                </div>                
-            </div>
-            <div className="py-5">
+        <section className="flex flex-col  gap-15 w-full h-full">
+            <div className="relative flex flex-col h-[50rem] w-full items-center justify-center bg-white">
+                  <div
+                    className={cn(
+                      "absolute inset-0",
+                      "[background-size:20px_20px]",
+                      "[background-image:radial-gradient(#b2b2b2_1px,transparent_1px)]",
+                    )}
+                  />
+                  {/* Radial gradient for the container to give a faded look */}
+                  <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] "></div>
+                <div className="relative flex flex-col gap-8 items-center">
+                    <h1 className="text-center xl:text-7xl text-6xl font-extrabold tracking-tight font-sans max-w-2xl">
+                        Democratizing IT {' '}                    
+                        <span className="bg-gradient-to-r from-blue-700 via-purple-700 to-red-700 bg-clip-text text-transparent">Beyond Barriers</span>
+                    </h1>    
+                    <h2 className="text-center font-light text-gray-900 tracking-wider">
+                        We believe in making technology accessible to everyone,<br></br> regardless of location, expertise, or resources.
+                    </h2>
+                    {/* <DotBackgroundDemo /> */}
+                    <div className="flex flex-row gap-5 justify-center font-bold">
+                        <Link href={'/#aboutus'}>
+                            <button className="flex bg-blue-600 py-2 px-5 rounded-full text-white cursor-pointer hover:scale-105 duration-150">About Us <MoveRight className="pl-2"/></button>
+                        </Link>
+                        <Link href={'/contact'}>
+                            <button className="flex bg-gray-100 py-2 px-5 rounded-full text-black cursor-pointer hover:scale-105 duration-150">Contact Us <MoveRight className="pl-2"/></button>
+                        </Link>
+                    </div>                
+                </div>
+                <div className="pt-15">
                     <div className="flex flex-col gap-5 items-center">
-                        <h1 className="text-xl font-light text-center text-black tracking-wide">Trusted by customers worldwide</h1>
+                        <h1 className="font-light text-center text-gray-900 tracking-wider">Trusted by customers worldwide</h1>
                         <LogoSlider />
                     </div>
                 </div>
+            </div>
         </section>
     )
 }
--- components/ui/timeline.tsx
+++ components/ui/timeline.tsx
@@ -1,6 +1,5 @@
 "use client";
 import {
-  useMotionValueEvent,
   useScroll,
   useTransform,
   motion,
@@ -34,10 +33,10 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
 
   return (
     <div
-      className="w-full bg-gray-100 font-sans md:px-10 overflow-clip"
+      className="w-full font-sans md:px-10 overflow-clip"
       ref={containerRef}
     >
-      <div className="max-w-7xl flex items-center justify-center mx-auto pt-15 px-4 md:px-8 lg:px-10">
+      <div className="max-w-7xl flex items-center justify-center mx-auto pt-10 px-4 md:px-8 lg:px-10">
         <h2 className="text-2xl md:text-4xl font-bold text-center mb-4 text-black max-w-4xl">
           Zeta-V&apos;s Journey Over the Years
         </h2>
